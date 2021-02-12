7acd1305774c531570d496827468684cfbbfe0f7
Kannan G <kannan.ganesan@thoughtworks.com>
2021-02-12 17:56:40 +0530

    Application property added

--- QuicLog.Api/Startup.cs
+++ QuicLog.Api/Startup.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Serilog;
+//using Serilog;
 
 namespace QuicLog.Api
 {
